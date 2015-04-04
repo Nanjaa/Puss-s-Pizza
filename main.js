87a24f09f60af60336587d77bc6e3d0819083bf1
Stephanie Olfert <steph.o-91@hotmail.com>
2015-04-04 15:07:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+// Menu
+
+$(document).ready(function() {
+	$('.clickdrinks').click(function() {
+
+		$('.drinksmenu').slideToggle('fast');
+	});
+
+	$('.clicksides').click(function() {
+		$('.sidesmenu').slideToggle('fast');
+	});
+
+	$('.clickbreakfast').click(function() {
+		$('.breakfastmenu').slideToggle('fast');
+	});
+
+	$('.clicklunch').click(function() {
+		$('.lunchmenu').slideToggle('fast');
+	});
+
+	$('.clickdinner').click(function() {
+		$('.dinnermenu').slideToggle('fast');
+	});
+
+	$('.clickdessert').click(function() {
+		$('.dessertmenu').slideToggle('fast');
+	});
+
+});
+
+
+//  Locations
---
+++ menu.js
@@ -0,0 +1,29 @@
+// Deserts
+
+$(document).ready(function() {
+	$('.clickdrinks').click(function() {
+
+		$('.drinksmenu').slideToggle('fast');
+	});
+
+	$('.clicksides').click(function() {
+		$('.sidesmenu').slideToggle('fast');
+	});
+
+	$('.clickbreakfast').click(function() {
+		$('.breakfastmenu').slideToggle('fast');
+	});
+
+	$('.clicklunch').click(function() {
+		$('.lunchmenu').slideToggle('fast');
+	});
+
+	$('.clickdinner').click(function() {
+		$('.dinnermenu').slideToggle('fast');
+	});
+
+	$('.clickdessert').click(function() {
+		$('.dessertmenu').slideToggle('fast');
+	});
+
+});
